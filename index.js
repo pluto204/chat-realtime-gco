d829aeae2607fff6712099d30a0cb4ad49813423
Hai <haitran@Hais-MacBook-Pro.local>
2020-07-13 15:11:12 +0700

    edit code chat

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var path = require("path");
+var express = require("express");
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//Chỉ ra đường dẫn chứa css, js, images...
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Tạo router
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/chat.html'));
+});
+
+//Tạo socket 
+io.on('connection', function (socket) {
+    console.log('Welcome to server chat');
+
+    socket.on('send', function (data) {
+        io.sockets.emit('send', data);
+    });
+});
+
+//Khởi tạo 1 server listen tại 1 port
+server.listen(8888);
